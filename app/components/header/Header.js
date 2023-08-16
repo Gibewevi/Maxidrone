14fb666126fe10d5a6bc709acd03ce8425ca5bba
gibewevi <gibewevi@skiff.com>
2023-08-16 16:14:52 +0200

    update

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -2,15 +2,15 @@ import Title from "./Title";
 
 export default function Headers() {
     return (
-        <div className="z-50 bg-white fixed w-full h-[90px] p-4">
-            <div className="bg-white max-w-5xl left-0 right-0 ml-auto mr-auto h-full flex flex-row gap-x-5 justify-between items-center">
+        <div className="z-50 bg-white abolute top-0 fixed w-full h-[90px] p-4">
+            <div className="bg-white max-w-6xl left-0 right-0 ml-auto mr-auto h-full flex flex-row gap-x-5 justify-between items-center">
                 <img src="/images/MAXIDRONE-LOGO-OFFICIEL.png" alt="Maxidrone Logo" className=" h-[70px]" />
                 <ul className="flex flex-row w-full justify-around text-sm">
                     <Title name="DEVIS" />
                     <Title name="CONTACT" />
                     <Title name="A PROPOS" />
                     <Title name="CONFIDENTIALITE" />
-                    <button className="p-1 pr-2 pl-2 bg-sky-400 text-white hover:bg-sky-500">COMPTE PRO</button>
+                    <button className="p-1 pr-2 pl-2 bg-[#03A9F4] text-white hover:bg-sky-500">COMPTE PRO</button>
                 </ul>
             </div>
         </div>
--- app/components/map/MyMap.js
+++ app/components/map/MyMap.js
@@ -1,48 +1,142 @@
 "use client"
 // "use client"
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import MyExploitations from './MyExploitations';
 
-function MyMap({ data }) {
-    const map = useMap();
-    const [isClicked, setIsClicked] = useState(false);
+const exploitations = [
+    {
+        id: 1,
+        name: "GAEC Des Serres de la Farède",
+        owner: "Jean Dupont",
+        coordinates: [44.417690, 2.009127],
+        date: ['01/01/2018', '01/01/2019', '01/01/2020']
+    },
+    {
+        id: 2,
+        name: "Lescure-Jaoul",
+        owner: "Jean Dupont",
+        coordinates: [44.366116, 2.064557],
+        date: ['01/01/2018', '01/01/2019', '01/01/2020', '01/01/2021', '01/01/2022']
+    },
+    {
+        id: 3,
+        name: "GAEC de Rouergue",
+        owner: "Jean Dupont",
+        coordinates: [44.371274, 1.991126],
+        date: ['01/01/2018', '01/01/2019', '01/01/2020', '01/01/2021']
+    },
+    {
+        id: 4,
+        name: "Exploitation Aveyronnaise",
+        owner: "Jean Dupont",
+        coordinates: [44.345746, 1.991469],
+        date: ['01/01/2018', '01/01/2019', '01/01/2020', '01/01/2021', '01/01/2022', '01/01/2023']
+    }
+];
 
-    const marketLocation = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="#03A9F4" d="M16 2A11.013 11.013 0 0 0 5 13a10.889 10.889 0 0 0 2.216 6.6s.3.395.349.452L16 30l8.439-9.953c.044-.053.345-.447.345-.447l.001-.003A10.885 10.885 0 0 0 27 13A11.013 11.013 0 0 0 16 2Zm0 15a4 4 0 1 1 4-4a4.005 4.005 0 0 1-4 4Z"/><circle cx="16" cy="13" r="4" fill="none"/></svg>`;
+function MyMap() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [exploitationId, setExploitationId] = useState(null);
 
-    const icon = L.divIcon({
-        className: "my-custom-pin",
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-        popupAnchor: [0, -32],
-        html: marketLocation
-    });
+    const getBounds = () => {
+        const lats = exploitations.map(exp => exp.coordinates[0]);
+        const longs = exploitations.map(exp => exp.coordinates[1]);
 
-    useEffect(() => {
-        L.marker(data.coordinates, { icon: icon }).addTo(map).bindPopup(`${data.name}<br>Propriétaire: ${data.owner}`);
-    }, [map, icon, data]);
+        return [
+            [Math.min(...lats), Math.min(...longs)],
+            [Math.max(...lats), Math.max(...longs)]
+        ];
+    };
 
-    return null;
-}
+    function FlyToExploitation({ exploitationId }) {
+        const map = useMap();
+    
+        useEffect(() => {
+            if (exploitationId) {
+                const selectedExploitation = exploitations.find(exp => exp.id === exploitationId);
+                if (selectedExploitation) {
+                    map.flyTo(selectedExploitation.coordinates, 14);
+                }
+            } else {
+                map.fitBounds(getBounds());
+            }
+        }, [exploitationId, map]);
+    
+        return null;
+    }
+    
+      
+    const handleExploitation = (exploitationId) => {
+        setIsOpen(!isOpen);
+        setExploitationId(exploitationId);
+    };
 
-function MyMapComponent() {
-    const position = [46.603354, 1.888334];
+    const handleSelectChange = (event) => {
+        const selectedExploitationId = parseInt(event.target.value);
+        setExploitationId(selectedExploitationId);
+        setIsOpen(true);
+    };
+
+
+    const ExploitationDate = (id) => {
+        const exploitation = exploitations.find(exp => exp.id === id);
+        if (exploitation) {
+            return exploitation.date.map((date, index) => (
+                <div className='flex flex-row gap-x-3'>
+                    <span>prospect_{date}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32"><path fill="#03A9F4" d="M13 21h13.17l-2.58 2.59L25 25l5-5l-5-5l-1.41 1.41L26.17 19H13v2z" /><path fill="#03A9F4" d="M22 14v-4a1 1 0 0 0-.29-.71l-7-7A1 1 0 0 0 14 2H4a2 2 0 0 0-2 2v24a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2h-2v2H4V4h8v6a2 2 0 0 0 2 2h6v2Zm-8-4V4.41L19.59 10Z" /></svg>
+                </div>
+            ));
+        }
+        return null; // Si aucune exploitation ne correspond à l'ID
+    };
 
     return (
-        <div className='max-w-5xl mx-auto rounded-xl overflow-hidden'>
-            <MapContainer center={position} zoom={6} style={{ width: '100%', height: '800px', zIndex: '0' }}>
+        <div className="max-w-5xl mx-auto rounded-xl relative overflow-x-hidden overflow-hidden">
+            <MapContainer
+                zoom={6}
+                style={{
+                    width: '100%',
+                    height: 'calc(100vh - 100px)',
+                    zIndex: '0'
+                }}
+                >
                 <TileLayer
                     url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
                     attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
                 />
-                <MyExploitations />
+                <MyExploitations exploitations={exploitations} handleExploitation={handleExploitation} />
+                <FlyToExploitation exploitationId={exploitationId} />
             </MapContainer>
+
+
+            <div className="absolute top-3 left-[60px] z-10">
+                <select className="p-2 rounded" onChange={handleSelectChange}>
+                    {exploitations.map((exploitation) => (
+                        <option key={exploitation.id} value={exploitation.id}>
+                            {exploitation.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className={`transform ${isOpen ? 'translate-x-0' : 'translate-x-[250px]'} duration-500 transition-transform ease-in-out flex flex-col items-center z-10 absolute top-0 right-0 w-[250px] h-[800px] bg-[white] border border-1 border-slate-100`}>
+                <div className='w-full flex flex-row gap-x-2 items-center justify-center bg-[#03A9F4] p-4 h-[40px] shadow-lg'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32"><path fill="white" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2.002 2.002 0 0 0-2-2h-4 a2.002 2.002 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z" /></svg>
+                    <span className='text-white font-bold text-md'>Prospection</span>
+                </div>
+                <div className='flex flex-col items-center gap-2 mt-5'>
+                    {exploitationId && ExploitationDate(exploitationId)}
+                </div>
+            </div>
         </div>
     );
+
 }
 
-export default MyMapComponent;
+export default MyMap;
 
 
